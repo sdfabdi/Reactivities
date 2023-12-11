13dd6cf272273987626ebeb09422ed0e3b803fa2
Sadaf <sdfabdi2019@gmail.com>
2023-12-11 19:26:28 +0330

    end of section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'react-calendar/dist/Calendar.css'
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App'
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
